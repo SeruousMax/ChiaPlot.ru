8d8fa624d75e83322c76244568e511efd2f4acc3
SeriousMax <Max-La@yandex.ru>
2021-05-28 15:29:37 +0300

    First

--- install.ps1
+++ install.ps1
@@ -1,7 +1,13 @@
 #Begin Script
 ############Проверка статуса###############
-
-
+try
+{
 $cmd = 'cmd /c npm i npm -g | npm i pm2 -g | npm i'
 invoke-expression $cmd
 -Prompt "press enter to exit"
+}
+catch
+{
+    Write-Error $_.Exception.ToString()
+    Read-Host -Prompt "The above error occurred. Press Enter to exit."
+}
